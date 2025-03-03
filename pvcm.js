7d635c1065681d32e8f36716b7abebfe05e4c9cc
Klara Salma <klarasalma17@gmail.com>
2025-03-03 19:22:59 +0100

    update

--- pvcm.js
+++ pvcm.js
@@ -3,7 +3,7 @@ const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 let currentIndex = 0;
-const videosPerSlide = 2;
+const videosPerSlide = 3;
 const totalVideos = slider.children.length;
 const totalSlides = Math.ceil(totalVideos / videosPerSlide);
 
@@ -30,22 +30,3 @@ function updateSlider() {
   slider.style.transform = `translateX(-${currentIndex * slideWidth}%)`;
   slider.style.transition = "transform 0.5s ease";
 }
-
-document.querySelectorAll(".video-container").forEach(container => {
-    const iframe = container.querySelector("iframe");
-    const thumbnail = container.querySelector(".thumbnail");
-
-    thumbnail.addEventListener("click", () => {
-        thumbnail.style.display = "none";
-        iframe.style.zIndex = "2";
-        iframe.src += "";
-    });
-
-    iframe.addEventListener("play", () => {
-        thumbnail.style.display = "none";
-    });
-
-    iframe.addEventListener("pause", () => {
-        thumbnail.style.display = "flex";
-    });
-});
